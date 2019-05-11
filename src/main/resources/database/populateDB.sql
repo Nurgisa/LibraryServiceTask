8b95a12fec5e10fe09abac581f5ec7c3737079b3
Nurgisa <nurgisa.seraly@gmail.com>
2019-05-11 07:46:01 +0600

    PopulateDb

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,5 +1,2 @@
 INSERT INTO authors (id,name) VALUES (1,'Gregory David Roberts');
 
-
-INSERT INTO books (id,isbn,title,author,date_published,genre) VALUES
-(1,'1-920769-00-5', 'Shantaram (novel)',1,'01.01.2003','adventure');
